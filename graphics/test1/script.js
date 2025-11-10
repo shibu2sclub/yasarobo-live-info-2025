d0d7cb12b45c8f935700bbc1cce2b4a8e9ecb189
Makoto Yamashita <makochan12.9@gmail.com>
2025-11-10 21:11:54 +0900

    change ID

--- graphics/test1/script.js
+++ graphics/test1/script.js
@@ -1,11 +1,11 @@
 // graphics/test1/script.js
 (function () {
     const vis = nodecg.Replicant('graphicsVisibility');
-    const root = document.getElementById('root');
+    const visibleRoot = document.getElementById('visible-root');
 
     vis.on('change', (v = {}) => {
         const visible = !!v.test1;
-        root.classList.toggle('active', visible);
+        visibleRoot.classList.toggle('active', visible);
         // root.classList.toggle('visible', visible); // ← visibleも使いたかったらこれもON
     });
 })();
